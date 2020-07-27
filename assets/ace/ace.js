649357d441752ead7ae3258dbc011cf64267fc1b
ccolombijn <ccolombijn@gmail.com>
2020-07-27 12:33:15 +0200

    Update options.with.prototype

--- assets/ace/ace.js
+++ assets/ace/ace.js
@@ -19,7 +19,6 @@ export function ace(options, next) {
       config.with.forEach((item) => options.with.unshift(item)); // merge config with options
     if (typeof options === 'function') {
       options();
-<<<<<<< HEAD
     }else{
       if(options.with.prototype === Array){
         options.with.forEach((item)=>{
@@ -30,15 +29,6 @@ export function ace(options, next) {
         }
         });
         
-=======
-    } else {
-      if (options.with.prototype === Array) {
-        options.with.forEach((item) => component.with(item));
-        view[item.name] = output;
-        controller[item.name] = {
-          model: item.model.name,
-        };
->>>>>>> 51f53fb1c95a6896bac6949b68704648742bd298
       }
     }
     if (typeof next === 'function') next(options);
